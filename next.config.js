32c1381c130117267641ce5fcd65002141600ed3
linus5304 <58887134+linus5304@users.noreply.github.com>
2022-12-12 00:54:33 +0100

    Initial commit: Setting up backend with AWS amplify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // experimental: {
+  //   appDir: true,
+  // },
+  images: {
+    domains: ['upload.wikimedia.org', 'commons.wikimedia.org']
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
